52b2c78bb4a56214a863e5f3262379fd7e6f54bc
najcjbd <zhubingchi27@163.com>
2026-02-19 17:33:53 +0800

    fix: resolve repository configuration conflict
    - Add JitPack repository to settings.gradle.kts
    - Remove repositories from build.gradle.kts buildscript
    - Remove repositories from build.gradle.kts allprojects
    - Fixes FAIL_ON_PROJECT_REPOS conflict error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
@@ -14,12 +6,6 @@ plugins {
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-    
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
     configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
